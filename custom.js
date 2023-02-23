fd278fa19a74f877b38ac21538ae39e5a6be09df
Abhishek Mohajan <123286153+Abhishek-Mohajan-projects@users.noreply.github.com>
2023-02-23 16:56:31 +0600

    Add files via upload

---
+++ custom.js
@@ -0,0 +1,18 @@
+"use strict";
+
+var boldText = document.querySelector(".text-bold");
+
+boldText.style.fontSize = "5rem";
+
+boldText.style.color = "cyan";
+
+boldText.style.letterSpacing = "0.4rem";
+
+
+var smallText = document.querySelector(".text-small");
+
+smallText.style.lineHeight = "2rem";
+
+var lineHeight = document.querySelector(".line-height");
+
+lineHeight.style.lineHeight = "2rem";
